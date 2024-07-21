17a3d2facfc990ec4771468d9a63d924c9297f2f
Jaymin Darji <jaymindarji2003@gmail.com>
2024-07-21 18:18:49 +0530

    learned lot's of things today

--- lec 8 let and const/scope.js
+++ lec 8 let and const/scope.js
@@ -3,9 +3,12 @@
 // Q1. Are let and const declaration hoisted?
 //* ans -> yes, let and const declaration hoisted but different than var declaration
 
-console.log(b); // undefined
-console.log(a); // error : can't access before initialization 
+//console.log(b); // undefined
+//console.log(a); // error : can't access before initialization 
 // console.log(c); // error : can't access before initialization
+
 let a = 10;
 var b = 100;
 const c = 1000;
+console.log("hello world");
+console.log(this.a)
---
+++ lec 9 block scope/scope.js
@@ -0,0 +1,14 @@
+
+let b = 1000;
+function x() {
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+
+// console.log(a)
+console.log(b)
+// console.log(c)
